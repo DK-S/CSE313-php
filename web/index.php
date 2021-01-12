474f7eab4689dcee6d46868c20184405fd1ea2e4
DK <dkillpack@gmail.com>
2021-01-12 13:14:49 -0700

    MyPage

--- web/index.php
+++ web/index.php
@@ -1,5 +1,20 @@
 <?php
 
-  phpinfo();
+  session_start();
 
+  $action = filter_input(INPUT_POST, 'action');
+  if ($action == NULL){
+    $action = filter_input(INPUT_GET, 'action');
+  }
+
+  switch ($action){
+    case 'home':
+      include 'view/home.php';
+      break;
+    case 'assignments':
+      include 'view/assignments.php';
+      break;
+    default:
+      include 'view/home.php';
+  }
 ?>
