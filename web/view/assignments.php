07236b78eeacb14039ddb016689f98dbe48c81b2
DK <dkillpack@gmail.com>
2021-01-14 13:46:17 -0700

    a

---
+++ web/view/assignments.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="/css/styles.css" media="screen">
+  </head>
+  <body>
+    <header>
+      <?php include $_SERVER['DOCUMENT_ROOT'].'/common/header.php';?>
+    </header>
+    <nav>
+      <?php echo $navList; ?>
+    </nav>
+    <h1>Donivan Killpack</h1>
+    <footer>
+      <?php include $_SERVER['DOCUMENT_ROOT'].'/common/footer.php';?>
+    </footer>
+  </body>
+</html>
