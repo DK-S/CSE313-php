7b8434619544a63d5989cc12c646733a5510c103
DK <dkillpack@gmail.com>
2021-01-14 13:51:49 -0700

    a

--- web/index.php
+++ web/index.php
@@ -2,10 +2,15 @@
 
   session_start();
 
+  //get the functions library
+  require_once 'library/functions.php';
+
   $action = filter_input(INPUT_POST, 'action');
   if ($action == NULL){
     $action = filter_input(INPUT_GET, 'action');
   }
+  $navList = getNavlist($action);
+
 
   switch ($action){
     case 'home':
