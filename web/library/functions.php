59909038144e02183987f6058cbfc915a3b0ea90
DK <dkillpack@gmail.com>
2021-01-14 19:28:05 -0700

    a

--- web/library/functions.php
+++ web/library/functions.php
@@ -6,15 +6,15 @@ function getNavlist($selected=''){
   
   $navList .= '<li';
   if($selected=='Home'){$navList .= ' class="Selected"';}
-  $navList .= '><a href="/web/" title="Home Link">Home</a></li>';
+  $navList .= '><a href="./" title="Home Link">Home</a></li>';
   
   $navList .= '<li';
   if($selected=='Assignments'){$navList .= ' class="Selected"';}
-  $navList .= '><a href="/web/?action=assignments" title="Assignments Link">Assignments</a></li>';
+  $navList .= '><a href="./?action=assignments" title="Assignments Link">Assignments</a></li>';
   
   $navList .= '<li';
   if($selected=='About'){$navList .= ' class="Selected"';}
-  $navList .= '><a href="/web/?action=about" title="About Link">About</a></li>';
+  $navList .= '><a href="./?action=about" title="About Link">About</a></li>';
   
   $navList .= '</ul>';
   return $navList;
