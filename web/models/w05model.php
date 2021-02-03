3dc8feaf4cdbb5452c114e96bc26050f9c428144
DK <dkillpack@gmail.com>
2021-02-02 17:43:46 -0700

    a

--- web/models/w05model.php
+++ web/models/w05model.php
@@ -3,8 +3,9 @@
 function getUsers(){
   $db = dbConnect();
   $sql = 'SELECT * FROM users';
-  $stmt = $db->prepare($sql);
-  $stmt->execute();
+  //$stmt = $db->prepare($sql);
+  //$stmt->execute();
+  $stmt = $db->query($sql);
   $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
   $stmt->closeCursor();
   return $rows;
